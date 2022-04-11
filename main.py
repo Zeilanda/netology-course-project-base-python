d0c3104ae6388a94bc7d74bae5385714afbd0122
Zeilanda <83118578+Zeilanda@users.noreply.github.com>
2022-04-11 18:21:33 +0300

    added choice of amount of photos

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import requests
 import time
 from progress.bar import IncrementalBar
 from my_token import token_vk, token_ya
+import json
 
 
 class VkDownloader:
     def __init__(self, token):
         self.token = token
 
-    def photos_download(self, user_id: str):
+    def photos_download(self, user_id: str, amount: str):
 
         params = {'v': '5.131', 'access_token': {self.token}, 'owner_id': user_id, 'album_id': 'profile',
-                  'extended': '1', 'photo_sizes': '1'}
+                  'extended': '1', 'photo_sizes': '1', 'count': amount}
 
         res = requests.get('https://api.vk.com/method/photos.get', params=params).json()
         info = res['response']['items']
@@ -32,6 +33,11 @@ class VkDownloader:
             time.sleep(0.5)
 
         bar_download.finish()
+
+        with open('json.txt', 'w') as file:
+            json_data = json.dumps(photos_url_list, ensure_ascii=False, indent=3)
+            file.write(json_data)
+
         return photos_url_list
 
 
@@ -50,30 +56,46 @@ class YaUploader:
         else:
             print('Ошибка в создании папки')
 
-    def upload(self, folder_name, file_name: str, file_url: str):
+    def upload(self, photo_list: list[dict], folder_name: str):
         """Метод загружает файлы по URL на яндекс диск"""
-        headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
+        bar_upload = IncrementalBar('Прогресс загрузки фотографий на Я.Диск:', max=len(photo_list))
+        for photo_dict in photo_list:
+            if 'file_name' and 'url' in photo_dict.keys():
+                bar_upload.next()
+                headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
+                file_path = folder_name + '/' + photo_dict['file_name']
+                params = {'path': file_path, 'url': photo_dict['url']}
+                url_file_upload = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+                requests.post(url_file_upload, headers=headers, params=params)
+                time.sleep(1)
 
-        file_path = folder_name + '/' + file_name
-        params = {'path': file_path, 'url': file_url}
-        url_file_upload = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-        requests.post(url_file_upload, headers=headers, params=params)
-        print('Files uploaded')
+            else:
+                print('Неверно сформированный json-файл')
+        bar_upload.finish()
+
+        headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
+        path = folder_name + '/json.txt'
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload?path=' + path
+        json_to_download = requests.get(url, headers=headers).json()
+        # print(f"link got: {json_to_download}")
+        with open('json.txt', 'r') as file:
+            json_file = file.read()
+        try:
+            resp = requests.put(json_to_download['href'], data=json_file)
+            print(f"upload result: {resp}")
+            return True
+        except KeyError:
+            print(json_to_download)
+            return False
 
 
 if __name__ == '__main__':
     owner_id = input('Введите идентификатор пользователя vkontakte: ')
+    photos_amount = input('Введите количество скачиваемых фотографий (по умолчанию 50): ')
     folder = input('Введите название папки для фотографий на Я.Диске: ')
     downloader = VkDownloader(token_vk)
-    photos_info = downloader.photos_download(owner_id)
+    photos_info = downloader.photos_download(owner_id, photos_amount)
 
     uploader = YaUploader(token_ya)
     uploader.create_folder(folder)
-    bar_upload = IncrementalBar('Прогресс загрузки фотографий на Я.Диск:', max=len(photos_info))
-
-    for photo_info in photos_info:
-        bar_upload.next()
-        uploader.upload(folder, photo_info['file_name'], photo_info['url'])
-        time.sleep(1)
-
-    bar_upload.finish()
+    uploader.upload(photos_info, folder)
